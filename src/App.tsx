18aafb569b1e21615403035c84acee4f6a961e76
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2021-03-18 22:31:38 -0300

    app start, top bar layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { StylesProvider } from '@material-ui/styles';
 import MyAppBar from './app-bar/AppBar';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <StylesProvider injectFirst>
-      <div className="App">
-        <MyAppBar></MyAppBar>
-      </div>
-    </StylesProvider>
+    <BrowserRouter>
+      <StylesProvider injectFirst>
+        <div id="theme-div" className="theme-dark">
+
+          <div className=" App">
+            <MyAppBar></MyAppBar>
+
+            <Switch>
+              <Route path="/test">
+                <div>Test</div>
+              </Route>
+              <Route path="/">
+                <div></div>
+              </Route>
+            </Switch>
+
+          </div>
+        </div>
+      </StylesProvider>
+
+    </BrowserRouter>
   );
 }
 
--- src/app-bar/AppBar.tsx
+++ src/app-bar/AppBar.tsx
@@ -1,21 +1,30 @@
-import AppBar from '@material-ui/core/AppBar';
-import { IconButton, Toolbar, Typography } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import './AppBar.css';
+import './AppBar.scss';
+
+interface BarButtonProps {
+    text: string;
+}
+
+function BarButton(props: BarButtonProps) {
+
+    return (
+        <div className='menu-text'>
+            {props.text}
+        </div>
+    )
+}
 
 export default function MyAppBar() {
     return (
         <div className="root">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className="title">
-                        Mateus Dutra Dadalto
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <div className="menu left">
+                <BarButton text="Mateus Dutra Dadalto"></BarButton>
+
+            </div>
+            <div className="menu right">
+                <BarButton text="About"></BarButton>
+                <BarButton text="Contact"></BarButton>
+                <BarButton text="Resume"></BarButton>
+            </div>
         </div>
     );
 }
