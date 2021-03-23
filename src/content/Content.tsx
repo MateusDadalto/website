3922f7ced0524222adb8791235eb07e968b6d24a
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2021-03-23 00:30:16 -0300

    Adding font and changing layout to grid

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -4,15 +4,7 @@ import './Content.scss'
 export default function Content() {
     return (
         <div className="content">
-            <div className='side-joke'>
-
-            </div>
-
-            <div className="info">
-                <Cover></Cover>
-            </div>
-
-            <div className='side-joke'></div>
+            <Cover />
         </div>
     );
 }
--- src/content/cover/Cover.tsx
+++ src/content/cover/Cover.tsx
@@ -1,10 +1,18 @@
 import './Cover.scss'
+import { ReactComponent as RocketOrbit } from './rocket.svg'
+import React from 'react';
 
 export default function Cover() {
-    return(
-        <div className="cover">
-            <div>text</div>            
-            <div>text2</div>
-        </div>
+    return (
+        <React.Fragment>
+            <div className="presentation">
+                <p>Hi, nice to meet you, my name is </p>
+                <h1 className="name">Mateus Dutra Dadalto</h1>
+                <p>I'm a software engineer based in Belo<br />Horizonte, Brazil</p>
+            </div>
+            <div className="rocket">
+                <RocketOrbit width='100%'></RocketOrbit>
+            </div>
+        </React.Fragment>
     );
 }
